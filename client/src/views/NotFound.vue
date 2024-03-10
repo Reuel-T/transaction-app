d9dab60e4680639ba101118c7941f3917d7e579c
Reuel Tyler <69512501+Reuel-T@users.noreply.github.com>
2024-03-10 16:41:49 +0200

    Styled Not Found page

--- client/src/views/NotFound.vue
+++ client/src/views/NotFound.vue
@@ -1,10 +1,23 @@
 <template>
-  <div class="page-card">
-    <h1>Route not Found</h1>
-    <RouterLink to="/">Home</RouterLink>
-  </div>
+  <AppPageWrapper>
+    <h1 class="title">Not Found</h1>
+    <h3>Hmmm... Can't seem to find what you're looking for...</h3>
+    <RouterLink
+          to="/"
+          role="button"
+        >
+          <button
+            id="back"
+            class="btn pill"
+          >
+            Back
+          </button>
+        </RouterLink>
+  </AppPageWrapper>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import AppPageWrapper from '@/components/global/AppPageWrapper.vue';
+</script>
 
 <style scoped></style>
