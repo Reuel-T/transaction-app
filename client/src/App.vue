9d4ea91a784e56755cf6c6be589410efaed83f66
Reuel Tyler <69512501+Reuel-T@users.noreply.github.com>
2024-03-10 16:52:39 +0200

    Route Transitions!

--- client/src/App.vue
+++ client/src/App.vue
@@ -6,8 +6,13 @@
 
 <template>
   <AppHeader />
+
   <div class="router-content">
-    <RouterView />
+    <RouterView v-slot="{ Component, route }">
+    <Transition :name="route.meta.transition ? 'route-back' : 'route'" mode="out-in">
+      <component :is="Component"></component>
+    </Transition>
+  </RouterView>    
   </div>
   <AppFooter />
 </template>
@@ -19,5 +24,39 @@
     align-items: center;
     justify-content: center;
     flex-grow: 1;
+  }
+
+  /* route transitions */
+  .route-enter-from {
+    opacity: 0;
+    transform: translateX(100px);
+  }
+  .route-enter-active {
+    transition: all 0.3s ease-out; 
+  }
+  .route-leave-to {
+    opacity: 0;
+    transform: translateX(-100px);
+  }
+  .route-leave-active {
+    transition: all 0.3s ease-in; 
+  }
+
+  .route-back-enter-from{
+    opacity: 0;
+    transform: translateX(-100px);
+  }
+
+  .route-back-enter-active{
+    transition: all 0.3s ease-out;
+  }
+
+  .route-back-leave-to{
+    opacity: 0;
+    transform: translateX(100px);
+  }
+
+  .route-back-leave-active{
+    transition: all 0.3s ease-in; 
   }
 </style>
--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -10,7 +10,8 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: ClientListView
+      component: ClientListView,
+      meta: {transition: 'route-back'}
     },
     {
       path: '/client/:id',
